337607f99d3552aa8ca87be180e0b0c70b98e1f5
Ubuntu <ubuntu@ip-172-31-25-106.cn-northwest-1.compute.internal>
2025-08-04 08:47:53 +0000

    chore: Ensure GPL-3.0 license compliance
    - Update package.json license field from MIT to GPL-3.0
    - Add GPL-3.0 license header to main server file
    - Maintain compliance with GNU General Public License v3.0
    
    This ensures the project properly declares its GPL-3.0 licensing
    and includes required copyright notices in source files.

--- server/index.js
+++ server/index.js
@@ -25,6 +25,24 @@ try {
 
 console.log('PORT from env:', process.env.PORT);
 
+/*
+ * Q Developer WebUI - A web-based interface for Amazon Q Developer CLI
+ * Copyright (C) 2024 Q Developer WebUI Contributors
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import express from 'express';
 import { WebSocketServer } from 'ws';
 import http from 'http';
